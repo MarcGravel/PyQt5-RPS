505325b9663d3dd46b8e27e727700c9fe4157d51
Marc Gravel <gravel.m@outlook.com>
2021-12-17 23:49:04 -0800

    added random image scramble on start game, stop game stops image scramble

--- mainRps.py
+++ mainRps.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import QTimer
+from random import randint
 
 class Window(QWidget):
     def __init__(self):
@@ -13,11 +14,14 @@ class Window(QWidget):
     #function to create UI widgets    
     def UI(self):
         ####Labels####
-        self.playerLabel = QLabel("Your Score: ", self)
+        self.startingPlayerScore = 0
+        self.startingComputerScore = 0
+        
+        self.playerLabel = QLabel("Your Score: "+str(self.startingPlayerScore), self)
         self.playerLabel.move(30, 20)
         self.playerLabel.setFont(QFont("Helvetica", 16))
         
-        self.computerLabel = QLabel("Computer Score: ", self)
+        self.computerLabel = QLabel("Computer Score: "+str(self.startingComputerScore), self)
         self.computerLabel.move(320, 20)
         self.computerLabel.setFont(QFont("Helvetica", 16))
         
@@ -26,12 +30,17 @@ class Window(QWidget):
         versusLabel.setFont(QFont("Helvetica", 25))
         
         ####Images####
+        self.rockImg = QPixmap("images/rock.png").scaledToWidth(150)
+        self.paperImg = QPixmap("images/paper.png").scaledToWidth(150)
+        self.scissorsImg = QPixmap("images/scissors.png").scaledToWidth(150)
+        
+        QPixmap("images/rock.png").scaledToWidth(150)
         self.playerImage = QLabel(self)
-        self.playerImage.setPixmap(QPixmap("images/rock.png").scaledToWidth(150))
+        self.playerImage.setPixmap(self.rockImg)
         self.playerImage.move(30, 100)
         
         self.computerImage = QLabel(self)
-        self.computerImage.setPixmap(QPixmap("images/rock.png").scaledToWidth(150))
+        self.computerImage.setPixmap(self.rockImg)
         self.computerImage.move(340, 100)
         
         ###Buttons###
@@ -45,10 +54,15 @@ class Window(QWidget):
         stopBtn.setFont(QFont("Arial", 13))
         stopBtn.clicked.connect(self.stopGame)
         
+        ###Win Message###
+        self.winMessage = QLabel("Test", self)
+        self.winMessage.move(200, 350)
+        self.winMessage.setFont(QFont("Helvetica", 40))
+        
         ####Timer####
         self.timer = QTimer(self)
-        self.timer.setInterval(1000)
-        self.timer.timeout(self.playing)
+        self.timer.setInterval(60)
+        self.timer.timeout.connect(self.playTheGame)
         
         #####show#####
         self.show()
@@ -56,12 +70,27 @@ class Window(QWidget):
     def startGame(self):
         self.timer.start()
     
+    def playTheGame(self):
+        self.randomComputerInt = randint(1, 3)
+        if self.randomComputerInt == 1: 
+            self.computerImage.setPixmap(self.rockImg)
+        elif self.randomComputerInt == 2: 
+            self.computerImage.setPixmap(self.paperImg)
+        elif self.randomComputerInt == 3: 
+            self.computerImage.setPixmap(self.scissorsImg)
+            
+        self.randomPlayerInt = randint(1, 3)
+        if self.randomPlayerInt == 1: 
+            self.playerImage.setPixmap(self.rockImg)
+        elif self.randomPlayerInt == 2: 
+            self.playerImage.setPixmap(self.paperImg)
+        elif self.randomPlayerInt == 3: 
+            self.playerImage.setPixmap(self.scissorsImg)
+    
     def stopGame(self):
         self.timer.stop()
-    
-    def playing(self):
-        pass
         
+    
 def main():
     App = QApplication(sys.argv)
     window = Window()
